10f99f2ac7c2750563b0a39c145e039abe0ead8d
Hebaadelenan <85418852+Hebaadelenan@users.noreply.github.com>
2022-07-01 22:30:22 +0200

    Update addbook_adduser.py

--- addbook_adduser.py
+++ addbook_adduser.py
@@ -6,3 +6,15 @@ def add_book(name,price,quantity):
 
     conn.commit()
     conn.close()
+    
+    
+    
+    
+def add_user(fname,lname, email,password, cash):
+    conn = sqlite3.connect('books.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO users VALUES ('{}','{}','{}','{}','{}')".format(fname,lname,email,password, cash))
+
+    conn.commit()
+    conn.close()
